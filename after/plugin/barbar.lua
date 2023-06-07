36853fbe29bfefe1351b128a8ed81a834ebc9600
Richard Harris <richard.harris@calendly.com>
2023-06-07 16:03:44 -0500

    messed with barbar a bit

--- after/plugin/barbar.lua
+++ after/plugin/barbar.lua
@@ -1,15 +1,10 @@
 vim.g.barbar_auto_setup = false -- disable auto-setup
 
 require'barbar'.setup {
-  -- WARN: do not copy everything below into your config!
-  --       It is just an example of what configuration options there are.
-  --       The defaults are suitable for most people.
-
-  -- Enable/disable animations
-  animation = true,
+  animation = false,
 
   -- Enable/disable auto-hiding the tab bar when there is a single buffer
-  auto_hide = false,
+  auto_hide = true,
 
   -- Enable/disable current/total tabpages indicator (top right corner)
   tabpages = true,
@@ -17,39 +12,20 @@ require'barbar'.setup {
   -- Enables/disable clickable tabs
   --  - left-click: go to buffer
   --  - middle-click: delete buffer
-  clickable = true,
-
-  -- Excludes buffers from the tabline
-  exclude_ft = {'javascript'},
-  exclude_name = {'package.json'},
-
-  -- A buffer to this direction will be focused (if it exists) when closing the current buffer.
-  -- Valid options are 'left' (the default), 'previous', and 'right'
-  focus_on_close = 'left',
+  clickable = false,
 
   -- Hide inactive buffers and file extensions. Other options are `alternate`, `current`, and `visible`.
-  hide = {extensions = true, inactive = true},
-
-  -- Disable highlighting alternate buffers
-  highlight_alternate = false,
-
-  -- Disable highlighting file icons in inactive buffers
-  highlight_inactive_file_icons = false,
-
-  -- Enable highlighting visible buffers
-  highlight_visible = true,
+  hide = {extensions = false, inactive = false},
 
   icons = {
     -- Configure the base icons on the bufferline.
     -- Valid options to display the buffer index and -number are `true`, 'superscript' and 'subscript'
-    buffer_index = false,
-    buffer_number = false,
-    button = '',
+    buffer_index = true,
     -- Enables / disables diagnostic symbols
     diagnostics = {
       [vim.diagnostic.severity.ERROR] = {enabled = true, icon = 'ﬀ'},
-      [vim.diagnostic.severity.WARN] = {enabled = false},
-      [vim.diagnostic.severity.INFO] = {enabled = false},
+      [vim.diagnostic.severity.WARN] = {enabled = true},
+      [vim.diagnostic.severity.INFO] = {enabled = true},
       [vim.diagnostic.severity.HINT] = {enabled = true},
     },
     gitsigns = {
@@ -83,22 +59,17 @@ require'barbar'.setup {
     visible = {modified = {buffer_number = false}},
   },
 
-  -- If true, new buffers will be inserted at the start/end of the list.
-  -- Default is to insert after current buffer.
-  insert_at_end = false,
-  insert_at_start = false,
-
   -- Sets the maximum padding width with which to surround each tab
-  maximum_padding = 1,
+  maximum_padding = 3,
 
   -- Sets the minimum padding width with which to surround each tab
-  minimum_padding = 1,
+  minimum_padding = 2,
 
   -- Sets the maximum buffer name length.
   maximum_length = 30,
 
   -- Sets the minimum buffer name length.
-  minimum_length = 0,
+  minimum_length = 5,
 
   -- If set, the letters for each buffer in buffer-pick mode will be
   -- assigned based on their name. Otherwise or in case all letters are
