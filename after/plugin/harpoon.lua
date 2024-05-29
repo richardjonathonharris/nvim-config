cdc778cdda5a8d70347da332aa10e751b50e4de2
richardjonathonharris <richard.harris@calendly.com>
2024-05-29 13:06:08 -0500

    have to shift over since we use <C-w> for pane navigation

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,9 +4,8 @@ harpoon:setup()
 vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
 vim.keymap.set('n', '<C-t>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 vim.keymap.set('n', '<C-q>', function() harpoon:list():select(1) end)
-vim.keymap.set('n', '<C-w>', function() harpoon:list():select(2) end)
-vim.keymap.set('n', '<C-e>', function() harpoon:list():select(3) end)
-vim.keymap.set('n', '<C-r>', function() harpoon:list():select(4) end)
+vim.keymap.set('n', '<C-e>', function() harpoon:list():select(2) end)
+vim.keymap.set('n', '<C-r>', function() harpoon:list():select(3) end)
 
 harpoon:extend({
     UI_CREATE = function(cx)
